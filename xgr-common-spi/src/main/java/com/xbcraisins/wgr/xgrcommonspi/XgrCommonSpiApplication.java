00bd3689ae3111a8297f02757e1bbd4a4182b7b7
weizeng <18550497663@sina.cn>
2019-02-18 16:56:11 +0800

    新建项目添加到本地仓库

---
+++ xgr-common-spi/src/main/java/com/xbcraisins/wgr/xgrcommonspi/XgrCommonSpiApplication.java
@@ -0,0 +1,13 @@
+package com.xbcraisins.wgr.xgrcommonspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgrCommonSpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XgrCommonSpiApplication.class, args);
+    }
+
+}
---
+++ xgr-common-spi/src/test/java/com/xbcraisins/wgr/xgrcommonspi/XgrCommonSpiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xbcraisins.wgr.xgrcommonspi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XgrCommonSpiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
