23cdb543dade83bb082c3f27de8ea2001d1b2f3e
xbcRaisins <38995527+xbcRaisins@users.noreply.github.com>
2019-03-12 23:09:23 +0800

    基础架构完成基本搭建工作，长路漫漫，勇往直前

---
+++ xgr-common-web/src/main/java/com/xbcraisins/wgr/xgrcommonweb/XgrCommonWebApplication.java
@@ -0,0 +1,13 @@
+package com.xbcraisins.wgr.xgrcommonweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgrCommonWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XgrCommonWebApplication.class, args);
+    }
+
+}
