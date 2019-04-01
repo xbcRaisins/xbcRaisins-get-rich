24720ca2c3dc9dde3b5dcc5194b0e90e4d9ba307
weizeng <18550497663@sina.cn>
2019-04-01 13:10:30 +0800

    nacos 注册中心

--- xgr-common-oauth/src/main/java/com/xbcraisins/wgr/xgrcommonoauth/XgrCommonOauthApplication.java
+++ xgr-common-oauth/src/main/java/com/xbcraisins/wgr/xgrcommonoauth/XgrCommonOauthApplication.java
@@ -2,10 +2,12 @@ package com.xbcraisins.wgr.xgrcommonoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableDiscoveryClient
 public class XgrCommonOauthApplication {
 
     public static void main(String[] args) {
--- xgr-common-web/src/main/java/com/xbcraisins/wgr/xgrcommonweb/XgrCommonWebApplication.java
+++ xgr-common-web/src/main/java/com/xbcraisins/wgr/xgrcommonweb/XgrCommonWebApplication.java
@@ -1,21 +1,56 @@
 package com.xbcraisins.wgr.xgrcommonweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.RequestContextListener;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class XgrCommonWebApplication extends SpringBootServletInitializer {
 
 
+    /**
+     * 启动http 监听 oauth2
+     * @date 2019/4/1 9:15
+     * @author xbcRaisins
+     * @param
+     * @return org.springframework.web.context.request.RequestContextListener
+     */
     @Bean
     public RequestContextListener requestContextListener() {
         return new RequestContextListener();
     }
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RestController
+    public class TestController {
+
+        private final RestTemplate restTemplate;
+
+        @Autowired
+        public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+        @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+        public String echo(@PathVariable String str) {
+            return restTemplate.getForObject("http://xgr-common-service/echo/" + str, String.class);
+        }
+    }
 
     public static void main(String[] args) {
 
