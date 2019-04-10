0717fa09c17d3a79fcc1fcb5bbfda289eff06211
xbcRaisins <38995527+xbcRaisins@users.noreply.github.com>
2019-04-10 22:45:08 +0800

    redis core

---
+++ xgr-common-core/src/main/java/com/xbcraisins/wgr/xgrcommoncore/XgrCommonCoreApplication.java
@@ -0,0 +1,13 @@
+package com.xbcraisins.wgr.xgrcommoncore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgrCommonCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XgrCommonCoreApplication.class, args);
+    }
+
+}
